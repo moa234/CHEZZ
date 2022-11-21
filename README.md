10938cd4222d5d3982b675ea7c579d55d4d459cd
Moaaz tarek <moaaztarek@hotmail.com>
2022-11-21 20:36:44 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# CMPN201 project
+- CHEZZ game rebuilt using assembly
+- 8086 processor game
---
+++ chat.asm
@@ -0,0 +1,12 @@
+.model 
+.data
+
+.code
+main proc far
+    ;set ds
+    mov ax,@data
+    mov ds,ax
+    
+    
+main endp
+end main
