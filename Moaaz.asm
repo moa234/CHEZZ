b9f16ac6a352e0021b9f05d1024f9361877d7397
Moaaz Tarek <moaaztarek@hotmail.com>
2022-12-03 11:45:06 +0200

    allow custom colour for rook piece

--- Moaaz.asm
+++ Moaaz.asm
@@ -13,15 +13,25 @@ main proc far
     mov ax,@data
     mov ds,ax
     
-    clearscreen
     GraphicsMode
+    mov ax,0700h
+    mov bh,00h
+    mov cx,0
+    mov dx,1827H
+    int 10h
     ;mov ah,0ch
     ;mov cx,8
     ;mov dx,5
     ;mov al,1
     ;int 10h
-    DrawRookBlue 32150
-    DrawRookRed 30000
+    DrawRook 32150,9h,1h
+    DrawRook 30000,0ch,4h
+    DrawRook 10000,0Dh,5h
+    DrawRook 20000,0Bh,3h
+    DrawRook 40000,0Ah,2h
+    DrawRook 25000,0Eh,6h
+    DrawRook 35000,0Fh,7h
+    
 
     ReadString InDATA
 
