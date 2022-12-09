752d5ddbeb59abad7107511e99ab043c01d8faa3
Moaaz Tarek <moaaztarek@hotmail.com>
2022-12-09 17:08:06 +0200

    highlight pawn & updated some traversing
    added col and row (0-7)
    added pixel col and pixel row

--- Moaaz.asm
+++ Moaaz.asm
@@ -8,6 +8,22 @@ Innum db 5,?,4 dup('$')
 currpos dw 0
 row dw 0
 col dw 0
+pixelrow dw 0
+pixelcol dw 0
+selectedpos dw 0
+selectedcol dw 0
+selectedrow dw 0
+highlightpos dw 0
+board db 8,9,10,11,12,10,9,8
+      db 7,7,7,7,7,7,7,7
+      db 0,0,0,0,0,0,0,0
+      db 0,0,0,0,0,0,0,0
+      db 0,0,0,0,0,0,0,0
+      db 0,0,0,0,0,0,0,0
+      db 1,1,1,1,1,1,1,1
+      db 6,5,4,3,2,4,5,6
+     
+
 
 include macros.inc
 include gameui.inc;comment from aley
@@ -19,17 +35,35 @@ main proc far
     mov ds,ax
     
     GraphicsMode
-    mov ax,0700h
-    mov bh,7h
-    mov cx,0
-    mov dx,1827H
-    int 10h
+    
 
 
     
     call initializegame
     whiletrue:
+        getKeyPress
+        jz whiletrue
+        clearbuffer
+        cmp al,'q'
+        je selectcell
         call traversecell
+
+        selectcell:
+        cmp al,'q'
+        jne whiletrue
+
+        mov bx,row
+        mov ax,8
+        mul bx
+        mov bx,ax
+        mov si,col
+        cmp board[bx][si],1
+        je highpawn
+
+        jmp whiletrue
+        highpawn:
+        call highlightpawn
+
     jmp whiletrue
 
 
